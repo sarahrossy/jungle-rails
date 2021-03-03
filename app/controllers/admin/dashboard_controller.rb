0984733522ee5a7277a8f3813ddebee5b7b32108
Sarah Rossy <sarahrossysings@gmail.com>
2021-03-03 23:22:18 +0000

    admin dashboard update, http auth env fix

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_CREDENTIAL_USERNAME'], password: ENV['BASIC_AUTH_CREDENTIAL_PASSWORD']
+
+  
   def show
-  end
+    @product_count = Product.count
+    @category_count = Category.count
+  end 
+
 end
